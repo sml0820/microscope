59c12e1bf049bad4e64da3c9bb0bb2e56ebd0301
Sean Lanning <sml0820@gmail.com>
2014-06-20 08:00:40 -0400

    add permissions to post collection

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -8,11 +8,11 @@ Template.postSubmit.events({
 			message: $(e.target).find('[name=message]').val()
 		};
 
-		Meteor.call('post', post, function(error,id){
-			if(error)
-				return alert(error.reason);
+        Meteor.call('post', post, function(error, id) {
+            if (error)
+                return alert(error.reason);
 
-			Router.go('postPage'), {_id: id};
-		});
+            Router.go('postPage', {_id: id});
+        });
 	}
 });
